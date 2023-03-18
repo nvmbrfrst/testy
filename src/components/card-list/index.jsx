ca910f2b772492fbc344839c26590ee919137b57
nvmbrfrst <mts89803448603@yandex.ru>
2023-03-18 18:26:33 +0300

    add cart

--- src/components/card-list/index.jsx
+++ src/components/card-list/index.jsx
@@ -1,10 +1,14 @@
-import './styles.css';
+import { dataCard } from "../../data";
+import { Card } from "../card";
+import "./styles.css";
 
 export function CardList() {
   return (
-    <>
-    
-    </>
+    <div className="cards content__cards">
+      {dataCard.map((dataItem) => (
+        <Card {...dataItem} />
+      ))}
+    </div>
   );
 }
 
