ca4b8bb81e8dff79b2113a03dfb5f1211cf1a05d
nvmbrfrst <mts89803448603@yandex.ru>
2023-03-19 09:10:44 +0300

    test

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -6,24 +6,50 @@ export function Card({ name, price, discount, wight, description, picture, ...pr
   console.log(props);
   return (
     <article className="card">
+
+      {/* иконка со скидкой */}
       <div className="card__sticky card__sticky_type_top-left">
         <span className="card__discount">{`-${discount}%`}</span>
       </div>
+
+      {/* иконка лайка  */}
       <div className="card__sticky card__sticky_type_top-right">
         <button className="card__favorite">
           <img src={likeIcon} alt="" className="card__favorite-icon" />
         </button>
       </div>
 
+      {/* карточка  */}
       <a href="#" className="card__link">
+
+        {/* карточка - картинка */}
         <img src={picture} alt={name} className="card__image" />
+
+        {/* описание */}
         <div className="card__desc">
-          <span className={discount !== 0 ? "card__old-price" : "card__price"}>{price}&nbsp;₽</span>
-          {discount !== 0 && <span className="card__price card__price_type_discount">{price}&nbsp;₽</span>}
+
+          {/* старая цена */}
+          <span className={discount !== 0 ? "card__old-price" : "card__price"}>
+            {price}&nbsp;₽
+          </span>
+
+          {/* скидка */}
+          {discount !== 0 && (
+            <span className="card__price card__price_type_discount">
+              {price}&nbsp;₽
+            </span>
+          )}
+
+          {/* вес */}
           <span className="card__wight">{wight}</span>
+
+          {/* имя карточки */}
           <h3 className="card__name">{name}</h3>
+
         </div>
       </a>
+
+      {/* кнопка в корзину */}
       <a href="#" className="card__cart btn btn_type_primary">В корзину</a>
     </article>
   );
