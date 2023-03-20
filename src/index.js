5f0a1a8e4a8cf1eff2821252fe5bf96d07677f9d
nvmbrfrst <mts89803448603@yandex.ru>
2023-03-20 16:58:43 +0300

    delete ant app

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from './components/app';
 import './styles.css'
-import { AntApp } from './components/app-ant';
+// import { AntApp } from './components/app-ant';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // подключение AntApp
-// root.render(<App />);
-root.render(<AntApp />);
+root.render(<App />);
+// root.render(<AntApp />);
 
 
 
