6a65ca8006a834b81f91c606c1a6c343302588ae
nvmbrfrst <mts89803448603@yandex.ru>
2023-03-20 19:04:20 +0300

    create cards

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from './components/app';
 import './styles.css'
+
+import { AppMui } from './components/app-mui';
 // import { AntApp } from './components/app-ant';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // подключение AntApp
-root.render(<App />);
 // root.render(<AntApp />);
+
+
+// подключение AppMui
+root.render(<AppMui />);
+
+
+// root.render(<App />);
+
 
 
 
