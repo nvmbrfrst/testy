1a4a9199a4ac83d8fd349f23039b7496bc33800b
nvmbrfrst <mts89803448603@yandex.ru>
2023-03-29 19:21:00 +0300

    fix button back

--- src/components/product/index.jsx
+++ src/components/product/index.jsx
@@ -6,8 +6,10 @@ import s from './styles.module.css';
 import { ReactComponent as LikeIcon } from "../../images/save.svg";
 import truck from "../../images/truck.svg";
 import quality from "../../images/quality.svg";
+import { useNavigate } from 'react-router-dom';
 
 function Product({ onProductLike, _id, name, pictures, description, discount, price, likes = [], currentUser, reviews }) {
+    const navigate = useNavigate();
     const discount_price = calcDiscountPrice(price, discount);
     const like = isLiked(likes, currentUser?._id);
     function handleLikeClick() {
@@ -21,7 +23,7 @@ function Product({ onProductLike, _id, name, pictures, description, discount, pr
     return (
         <>
             <div className={s.header}>
-                <a href="#" className='button-back'>Назад</a>
+                <a href="#" className='button-back' onClick={() => navigate(-1)}>Назад</a>
                 <h1 className={s.productTitle}>{name}</h1>
                 <p className={s.articul}>Артикул: <b>2388907</b></p>
             </div>
