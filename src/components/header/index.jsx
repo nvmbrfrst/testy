a7cfb15adbb2a88507a1a071b9adb1de3271214a
nvmbrfrst <mts89803448603@yandex.ru>
2023-04-04 17:43:31 +0300

    create likebutton - header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,36 +1,31 @@
 import cn from 'classnames';
 import { useContext } from 'react';
-// import { UserContext } from '../../contexts/current-user-context';
-// import { Button } from '../button';
+import { UserContext } from '../../contexts/current-user-context';
 
 import s from "./styles.module.css";
-// import "./styles.css";
-// import { ThemeContext } from '../../contexts/theme-context';
+import "./styles.css";
+import { CardsContext } from '../../contexts/card-context';
+import { Link } from 'react-router-dom';
+import { ReactComponent as FavoriteIcon } from './img/favorites.svg'
 
 export function Header({ children }) {
-  // const { currentUser, onUpdateUser } = useContext(UserContext);
-  // const { toggleTheme } = useContext(ThemeContext)
-
-  // const handleClickButtonEdit = () => {
-  //   onUpdateUser({ name: 'Вася', about: 'Ментор' })
-  // }
+  const { currentUser, onUpdateUser } = useContext(UserContext);
+  const { favorites } = useContext(CardsContext)
+  
+  const handleClickButtonEdit = () => {
+    onUpdateUser({ name: 'Вася', about: 'Ментор' })
+  }
 
   return (
     <header className={s.header}>
       <div className={cn('container', s.wrapper)}>
         {children}
-        {/* <span>{currentUser?.name}: {currentUser?.about}</span>
-        <span>{currentUser?.email}</span>
-        <Button action={handleClickButtonEdit}>
-          Изменить
-        </Button> */}
-        {/* <label className="wraper" htmlFor="something">
-          <div className="switch-wrap">
-            <input type="checkbox" id="something" onChange={toggleTheme} />
-            <div className="switch"></div>
-          </div>
-        </label> */}
-
+        <div className={s.iconsMenu}>
+          <Link className={s.favoritesLink} to={{ pathname: '/favorites' }}>
+            <FavoriteIcon />
+            {favorites.length !== 0 && <span className={s.iconBubble}>{favorites.length}</span>}
+          </Link>
+        </div>
       </div>
 
     </header>
