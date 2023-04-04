53bb016f37886f7efae8ec763407a939097219f9
nvmbrfrst <mts89803448603@yandex.ru>
2023-04-04 16:37:12 +0300

    перенаправить массивы в catalog-page

--- src/components/card-list/index.jsx
+++ src/components/card-list/index.jsx
@@ -3,8 +3,8 @@ import { CardsContext } from '../../contexts/card-context';
 import { Card } from "../card";
 import "./styles.css";
 
-export function CardList() {
-  const { cards: goods } = useContext(CardsContext)
+export function CardList({goods}) {
+  // const { cards: goods } = useContext(CardsContext)
   return (
     <div className="cards content__cards">
       {goods.map((dataItem, index) => (
--- src/pages/catalog-page/index.jsx
+++ src/pages/catalog-page/index.jsx
@@ -1,17 +1,20 @@
+import { useContext } from 'react';
 import { CardList } from '../../components/card-list'
 import { Sort } from '../../components/sort'
 import { Spinner } from '../../components/spinner';
 
 import s from './styles.module.css';
+import { CardsContext } from '../../contexts/card-context';
 
 export const CatalogPage = ({ isLoading }) => {
+    const { cards: goods } = useContext(CardsContext)
     return (
         <>
             {isLoading
                 ? <Spinner />
                 : <>
                     <Sort />
-                    <CardList />
+                    <CardList goods={goods} />
                 </>
             }
         </>
