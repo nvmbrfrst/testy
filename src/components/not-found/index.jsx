59776d20052d3804fce87a76747d99e56c94d0cf
nvmbrfrst <mts89803448603@yandex.ru>
2023-04-05 08:32:33 +0300

    test

--- src/components/not-found/index.jsx
+++ src/components/not-found/index.jsx
@@ -6,7 +6,7 @@ import s from "./styles.module.css";
 export function NotFound({ children, title, buttonText = "На главную", buttonAction }) {
   return (
     <div className={s.notfound}>
-      <NotFoundIcon className={s.image} />
+      <NotFoundIcon className={s.image} aria-hidden="true" />
       <h1 className={s.title}>{title}</h1>
       {children && children}
 
